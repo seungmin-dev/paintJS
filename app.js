53783c51436a698ec2685473bad7611a81e163c0
seungmin-dev <67530394+seungmin-dev@users.noreply.github.com>
2020-08-25 00:55:16 +0900

    Edit app.js

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "";
+const INTIAL_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = INTIAL_SIZE;
+canvas.height = INTIAL_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, INTIAL_SIZE, INTIAL_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
 	painting = false;
@@ -37,6 +47,41 @@ function onMouseDown(event) {
 function handlerColorClick(event){
 	const color = event.target.style.backgroundColor;
 	ctx.strokeStyle = color;
+	ctx.fillStyle = color;
+}
+
+function handlerRangeChange(event){
+	const size = event.target.value;
+	ctx.lineWidth = size;
+}
+
+function handlerChageMode() {
+	if(filling === true) {
+		filling = false;
+		mode.innerText = "Fill"
+	} else {
+		filling = true;
+		mode.innerText = "Paint";
+	}
+}
+
+function handlerCanvasClick() {
+	if(filling) {
+		ctx.fillRect(0, 0, INTIAL_SIZE, INTIAL_SIZE);
+	}
+
+}
+
+function handlerContextMenu(event) {
+	event.preventDefault();
+}
+
+function handlerSave() {
+	const image = canvas.toDataURL();
+	const link = document.createElement("a");
+	link.href = image;
+	link.download = "paintJS";
+	link.click();
 }
 
 if (canvas) {
@@ -44,7 +89,21 @@ if (canvas) {
 	canvas.addEventListener("mousedown", startPainting);
 	canvas.addEventListener("mouseup", stopPainting);
 	canvas.addEventListener("mouseleave", stopPainting);
+	canvas.addEventListener("click", handlerCanvasClick);
+	canvas.addEventListener("contextmenu", handlerContextMenu);
 }
 
 // console.log(Array.from(colors));
 Array.from(colors).forEach(color => color.addEventListener("click", handlerColorClick));
+
+if(range) {
+	range.addEventListener("input", handlerRangeChange);
+}
+
+if(mode) {
+	mode.addEventListener("click", handlerChageMode);
+}
+
+if(saveBtn) {
+	saveBtn.addEventListener("click", handlerSave);
+}
